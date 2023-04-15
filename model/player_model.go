ca445d57b95b75d8ef446e6ac227732442b7f8ae
Safat99 <sarkersafat99@gmail.com>
2023-04-15 15:56:05 +0600

    validation added in the code

--- model/player_model.go
+++ model/player_model.go
@@ -18,11 +18,11 @@ const (
 
 type Player struct {
 	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	PlayerName string             `json:"player_name,omitempty" bson:"player_name"`
-	Age        int64              `json:"age,omitempty" bson:"age"`
-	Position   Pos                `json:"position,omitempty" bson:"position"`
-	Country    string             `json:"country,omitempty" bson:"country"`
-	DOB        time.Time          `json:"date_of_birth" bson:"date_of_birth"`
+	PlayerName string             `json:"player_name" bson:"player_name" validate:"required"`
+	Age        int64              `json:"age" bson:"age" validate:"required,gte=0,lte=150"`
+	Position   Pos                `json:"position" bson:"position" validate:"required"`
+	Country    string             `json:"country" bson:"country" validate:"required"`
+	DOB        string             `json:"date_of_birth" bson:"date_of_birth" vaidate:"required,date"`
 	CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt  time.Time          `json:"updated_at" bson:"updated_at"`
 }
@@ -31,3 +31,5 @@ type UpdatePlayerDto struct {
 	PlayerName string `json:"player_name,omitempty" bson:"player_name,omitempty"`
 	Position   Pos    `json:"position,omitempty" bson:"position,omitempty"`
 }
+
+// func validateDateFormat(f1 validator.FieldLevel)
--- service/player_service.go
+++ service/player_service.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 	"player_info/model"
 	"player_info/repository"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type PlayerService struct {
@@ -21,8 +23,16 @@ func (p *PlayerService) Index(c *gin.Context) {
 	c.JSON(200, "welcome to the app")
 }
 
+var dob_regex = `^([0-9]{4}[-/]?((0[13-9]|1[012])[-/]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-/]?31|02[-/]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)[-/]?02[-/]?29)$`
+
+func ValidateString(input, dob_regex string) bool {
+	re := regexp.MustCompile(dob_regex)
+	return re.MatchString(input)
+}
+
 func (p *PlayerService) Create(c *gin.Context) {
 	var player model.Player
+
 	decoder := json.NewDecoder(c.Request.Body)
 	err := decoder.Decode(&player)
 	if err != nil {
@@ -31,6 +41,31 @@ func (p *PlayerService) Create(c *gin.Context) {
 		})
 		return
 	}
+	// validation
+	validate := validator.New()
+	if err := validate.Struct(player); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "validator worked",
+			"error":   err.Error(),
+		})
+		return
+	}
+	// custom validation
+	if player.PlayerName == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "player name cannot be empty",
+		})
+		return
+	}
+
+	isValid := ValidateString(player.DOB, dob_regex)
+	if !isValid {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "dob validation failed",
+		})
+		return
+	}
+
 	id, err := p.PlayerRepository.CreatePlayer(player)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -94,8 +129,8 @@ func (p *PlayerService) GetAll(c *gin.Context) {
 
 func (p *PlayerService) UpdatePlayer(c *gin.Context) {
 	id := c.Param("id")
-
 	var playerdto *model.UpdatePlayerDto
+
 	decoder := json.NewDecoder(c.Request.Body)
 	err := decoder.Decode(&playerdto)
 	if err != nil {
@@ -111,17 +146,20 @@ func (p *PlayerService) UpdatePlayer(c *gin.Context) {
 			"eror": err.Error(),
 		})
 	}
+
 	c.JSON(201, player)
 }
 
 func (p *PlayerService) DeletePlayerById(c *gin.Context) {
 	id := c.Param("id")
+
 	err := p.PlayerRepository.DeletePlayerById(id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"eror": err.Error(),
 		})
 	}
+
 	c.JSON(200, gin.H{
 		"message": "player deleted",
 	})
